9cdf5ae9278cf6467ce2f9419e13de3ada221787
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-24 16:14:05 +0530

    Add available operations to a dictionary with functions as values

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ def multiply(num_1, num_2):
 def divide(num_1, num_2):
     return num_1 / num_2
 
-def calculate(num_1, num_2):
-    if operation == "+":
-        return add(num_1, num_2)
-    elif operation == "-":
-        return subtract(num_1, num_2)
-    elif operation == "*":
-        return multiply(num_1, num_2)
-    elif operation == "/":
-        return divide(num_1, num_2)
-
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
 print("Welcome to The Python Calculator!")
 print(art.calculator)
 
@@ -27,9 +23,11 @@ should_continue = "n"
 while True:
     if should_continue == "n":
         num_1 = float(input("Enter the first number: "))
-        operation = input("+\n-\n*\n/\nPick an operation: ")
+        for operation in operations:
+            print(operation)
+        operation = input("Pick an operation: ")
         num_2 = float(input("Enter the second number: "))
-        result = calculate(num_1, num_2)
+        result = operations[operation](num_1, num_2)
 
         print(f"{num_1} {operation} {num_2} = {result}")
 
@@ -37,9 +35,11 @@ while True:
 
     elif should_continue == "y":
         num_1 = result
-        operation = input("+\n-\n*\n/\nPick an operation: ")
+        for operation in operations:
+            print(operation)
+        operation = input("Pick an operation: ")
         num_2 = float(input("Enter the second number: "))
-        result = calculate(num_1, num_2)
+        result = operations[operation](num_1, num_2)
         print(f"{num_1} {operation} {num_2} = {result}")
 
         should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
