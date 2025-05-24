6fc69d58d544bdee7fc5c6aebf68a7f3764fee10
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-24 15:59:05 +0530

    Add feature to continue calculating with result according to user choice

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import art
 def add(num_1, num_2):
     return num_1 + num_2
 def subtract(num_1, num_2):
-    return num_1 - num_1
+    return num_1 - num_2
 def multiply(num_1, num_2):
     return num_1 * num_2
 def divide(num_1, num_2):
@@ -24,18 +24,22 @@ print(art.calculator)
 
 should_continue = "n"
 
-while should_continue == "n":
-    num_1 = float(input("Enter the first number: "))
-    operation = input("+\n-\n*\n/\nPick an operation: ")
-    num_2 = float(input("Enter the second number:"))
-    result = calculate(num_1, num_2)
-
-    print(f"{num_1} {operation} {num_2} = {result}")
-
-    should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
-
-    if should_continue == "y":
+while True:
+    if should_continue == "n":
+        num_1 = float(input("Enter the first number: "))
         operation = input("+\n-\n*\n/\nPick an operation: ")
-        num_2 = float(input("Enter the second number:"))
-        new_result = calculate(result, num_2)
-        print(f"{result} {operation} {num_2} = {new_result}")
+        num_2 = float(input("Enter the second number: "))
+        result = calculate(num_1, num_2)
+
+        print(f"{num_1} {operation} {num_2} = {result}")
+
+        should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+
+    elif should_continue == "y":
+        num_1 = result
+        operation = input("+\n-\n*\n/\nPick an operation: ")
+        num_2 = float(input("Enter the second number: "))
+        result = calculate(num_1, num_2)
+        print(f"{num_1} {operation} {num_2} = {result}")
+
+        should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
