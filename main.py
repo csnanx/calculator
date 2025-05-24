ad420b683006b641043a903fa7bd4585ffe63e77
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-24 15:30:49 +0530

    Add feature to show result after calculation with previous result

--- main.py
+++ main.py
@@ -30,4 +30,7 @@ while should_continue == "n":
     should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
 
     if should_continue == "y":
-        result = calculate(result, num_2)
+        operation = input("+\n-\n*\n/\nPick an operation: ")
+        num_2 = float(input("Enter the second number:"))
+        new_result = calculate(result, num_2)
+        print(f"{result} {operation} {num_2} = {new_result}")
