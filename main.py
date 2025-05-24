e72af188d6057de72dc1c9eaf25cc1045d8ff71b
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-24 19:53:18 +0530

    Refactor the code by modifying repetitive code

--- main.py
+++ main.py
@@ -28,25 +28,15 @@ print(art.calculator)
 should_continue = "n"
 
 while True:
-    if should_continue == "n":
-        num_1 = float(input("Enter the first number: "))
-        for operation in operations:
-            print(operation)
-        operation = input("Pick an operation: ")
-        num_2 = float(input("Enter the second number: "))
-        result = operations[operation](num_1, num_2)
-
-        print(f"{num_1} {operation} {num_2} = {result}")
-
-        should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
-
-    elif should_continue == "y":
+    if should_continue == "y":
         num_1 = result
-        for operation in operations:
-            print(operation)
-        operation = input("Pick an operation: ")
-        num_2 = float(input("Enter the second number: "))
-        result = operations[operation](num_1, num_2)
-        print(f"{num_1} {operation} {num_2} = {result}")
+    elif should_continue == "n":
+        num_1 = float(input("Enter the first number: "))
+    for operation in operations:
+        print(operation)
+    operation = input("Pick an operation: ")
+    num_2 = float(input("Enter the second number: "))
+    result = operations[operation](num_1, num_2)
+    print(f"{num_1} {operation} {num_2} = {result}")
 
-        should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+    should_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
